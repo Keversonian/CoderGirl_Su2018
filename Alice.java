2d6b3b1439196a2dc7929d3ce5ea3e964bc9bdae
Keversonian <35574203+Keversonian@users.noreply.github.com>
2018-08-18 15:36:21 -0500

    Alice (Story TIme)

---
+++ Alice.java
@@ -0,0 +1,14 @@
+import static java.lang.System.out;
+public class Alice {
+    public static void main(String[] args)
+    {
+        // ---- Your code goes below this line ----
+        out.println("Alice was beginning to get very tired of sitting by her sister ");
+        out.print("on the bank, and of having nothing to do: once or twice she\n");
+        out.print("had peeped into the book her sister was reading, but it had\nno pictures or conversations in it, ");
+        out.println("\"and what is the use of\na book,\" thought Alice \"without any pictures or conversations?\"\n");
+        out.println("So she was considering in her own mind as (as well as she could,\nfor the hot day made her feel very sleepy and stupid), whether");
+        out.print("the pleasure of making a daisy-chain would be worth the trouble\nof getting up and picking the daisies, when suddenly a White\nRabbit with pink eyes ran close by her.");
+        // ---- Your code goes above this line ----
+    }
+}
